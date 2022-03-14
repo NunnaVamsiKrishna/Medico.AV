343ea14b8f04e025b44805612c3a95ed33c0c1ab
Vamsi <2000030732@kluniversity.in>
2022-03-14 13:58:14 +0530

    initial commit

---
+++ medico/medico/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+def index(request):
+    return render(request,"index.html")
+def login(request):
+    return render(request,"login.html")
+def dash(request):
+    return render(request,"dash.html")
+def base(request):
+    return render(request,"base.html")
